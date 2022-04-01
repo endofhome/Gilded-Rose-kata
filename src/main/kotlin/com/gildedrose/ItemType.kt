fd5ad73615ab6a4786b95a41a2afff111e62da66
Tom Barnsbury <ruralmyth@gmail.com>
2022-04-02 07:08:21 +1000

    Use defaults, no need to handle unknown item types.

--- src/main/kotlin/com/gildedrose/ItemType.kt
+++ src/main/kotlin/com/gildedrose/ItemType.kt
@@ -23,20 +23,19 @@ object ConcertTicketItemType : ItemType(
 )
 
 fun itemTypeFor(itemName: String): ItemType {
-    val prefixAndItem = itemName.split("Conjured")
+    // TODO the prefix is an empty string when present, which is a bit odd. Is there a better way to express this.
+    val prefixAndItem = itemName.split("Conjured").map { it.trim() }
     val (qualityDegradationFactor, itemNameWithoutPrefix) =
+        // TODO join non-prefixed items to string, don't take the first. Needs a test.
         when {
-            prefixAndItem.size == 2 && prefixAndItem.first().isEmpty() -> Pair(ConjuredQualityDegradationFactor, prefixAndItem[1].trim())
-            else                                                       -> Pair(DefaultQualityDegradationFactor, prefixAndItem[0].trim())
+            prefixAndItem.size == 2 && prefixAndItem.first().isEmpty() -> Pair(ConjuredQualityDegradationFactor, prefixAndItem[1])
+            else                                                       -> Pair(DefaultQualityDegradationFactor, prefixAndItem[0])
         }
 
     return when (itemNameWithoutPrefix) {
-            "+5 Dexterity Vest",
-            "Elixir of the Mongoose",
-            "Mana Cake"                                 -> DefaultItemType(qualityDegradationFactor)
-            "Aged Brie"                                 -> AgedBrieItemType
-            "Sulfuras, Hand of Ragnaros"                -> LegendaryItemType
-            "Backstage passes to a TAFKAL80ETC concert" -> ConcertTicketItemType
-            else                                        -> error("Unknown item type: \"$itemNameWithoutPrefix\"")
+        "Aged Brie"                                 -> AgedBrieItemType
+        "Sulfuras, Hand of Ragnaros"                -> LegendaryItemType
+        "Backstage passes to a TAFKAL80ETC concert" -> ConcertTicketItemType
+        else                                        -> DefaultItemType(qualityDegradationFactor)
     }
 }
